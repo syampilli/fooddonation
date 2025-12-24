3ecaa60bd55ead90fbf5f3b52bea827ab19034b8
syamsatyanarayanapilli <pillisyamsatyanarayana@gmail.com>
2025-12-25 03:43:27 +0530

    fp update

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+       service: "gmail", // true for 465, false for 587
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Food Donation App" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: `"Food Saver" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("📧 Email sent successfully");
+  } catch (error) {
+    console.error("❌ Email send failed:", error);
+    throw error; // VERY IMPORTANT
+  }
 };
 
 module.exports = sendEmail;
